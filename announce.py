dd650045caf82365ded657b552e9215643a9e5cf
Atharva <atharvachangole2965@gmail.com>
2026-07-09 16:28:02 +0530

    tried diffrent approch completely redaicate the tracker and its peer list and added the desktop qbit torrent client seeding my own torret file

--- announce.py
+++ announce.py
@@ -13,17 +13,17 @@ global num_pieces
 #constants
 PORT=6881
 
-with open('fight.club.10th.anniversary.edition.1999.1080p.brrip.x264.yify_201908_archive.torrent' , 'rb') as file:
+with open('test_file.bin.torrent' , 'rb') as file:
     data=file.read()
 # data=b'd4:name7:atharvai3e5:listl4:spam3:egge'
 index=0
 while index < len(data):# this code checks all the variation and doesn't let any of the object remain unread
     result, new_index=parser.decoder(data,index)
-    print(result)
-    announce=result[b"announce"].decode()
-    info=result[b'info']
+    #print(result)
+   # announce=result[b"announce"].decode()
+    #info=result[b'info']
     index=new_index
-url= announce
+#url= announce
 
 num_pieces=len(result[b'info'][b'pieces'] )//20 # for each peice consist 20 bytes of hash fucntion
 
@@ -61,20 +61,21 @@ params=\
 }
 
 # mild starting of getting peer list
-response=requests.get(url,params=params)
-print(response.url) # tracker sends the responsible data  and peer list in b encoded
-tracker_data,new_index=parser.decoder(response.content,0) # passing tracker data in  our deccoder
+#response=requests.get(url,params=params)
+#print(response.url) # tracker sends the responsible data  and peer list in b encoded
+#tracker_data,new_index=parser.decoder(response.content,0) # passing tracker data in  our deccoder
 
 
 # getting peer list
-peer_data=tracker_data[b'peers']# raw bytes from the tracker these have 6 bytes for each peer
+#peer_data=tracker_data[b'peers']# raw bytes from the tracker these have 6 bytes for each peer
 peers=[]# for storing peer ip and port
-for i in range(0,len(peer_data),6): # we need exact steping 6 bcz verything is clampppedin peer_data
-    ip_bytes=peer_data[i:i+4] # selecting first four bytes through slicing but need to join in ip format
-    ip=".".join(str(b) for b in ip_bytes) # this fucntion is similar to for b in ip_bytes
+#for i in range(0,len(peer_data),6): # we need exact steping 6 bcz verything is clampppedin peer_data
+   # ip_bytes=peer_data[i:i+4] # selecting first four bytes through slicing but need to join in ip format
+    #ip=".".join(str(b) for b in ip_bytes) # this fucntion is similar to for b in ip_bytes
                                                                         #   ip=b+"."
-    port_bytes=peer_data[i+4:i+6] # getting the port for that exact ip in that 6 ray byte range by grabing the exact
-    port=int.from_bytes(port_bytes,'big') # port_bytes contain the  raw bits it need to convert into the integers
-    peers.append((ip,port))
+   # port_bytes=peer_data[i+4:i+6] # getting the port for that exact ip in that 6 ray byte range by grabing the exact
+   # port=int.from_bytes(port_bytes,'big') # port_bytes contain the  raw bits it need to convert into the integers
+   # peers.append((ip,port))
+peers.append(("127.0.0.1", 6881))
 print(len(peers))
-print(result[b'announce-list'])
+# print(result[b'announce-list'])
